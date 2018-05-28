1e63b97d14812e5f393a4f83e64297eabeb3fc90
JRBenson01 <jribenson01@gmail.com>
2018-05-27 20:56:03 -0400

    Working game
    -Functioning circular collisions
    -Endzone
    -Win and Lose screens
    -AI to follow character

--- engineDev1.py
+++ engineDev1.py
@@ -1,10 +1,13 @@
 import pygame, sys
 from pygame.locals import *
+import time
+import math
 pygame.init()
 
 white = (255, 255, 255)
 black = (0, 0, 0)
 red = (255, 0, 0)
+dim_red = (125, 0, 0)
 orange = (255, 125, 0)
 yellow = (255, 255, 0)
 spring_green = (125, 255, 0)
@@ -27,6 +30,7 @@ class game_Window(object):
         self.frameCount = 0
         self.clock = pygame.time.Clock()
         self.fps = 0
+        self.fps_cap = 90
         self.use_background_image = False
         self.background_color = white
 
@@ -50,6 +54,9 @@ class game_Window(object):
     def set_background_color(self, color):
         self.background_color = color
 
+    def set_fps_cap(self, fps_cap):
+        self.fps_cap = fps_cap
+
     def gameExit(self):
         print("exiting game")
         pygame.quit()
@@ -57,15 +64,22 @@ class game_Window(object):
 
     def frameUpdate(self, scene):
         self.clock.tick()
-        self.fps = self.clock.get_fps() 
-        #print ("FPS: ", self.fps)
+        self.fps = self.clock.get_fps()
+        pygame.time.delay(int(1000/self.fps_cap))
+        fps_message = str("FPS: %s" % (int(self.fps)))
+        fps_display.set_text(fps_message)
         if scene == "Main":
             if self.use_background_image:
                 self.display.blit(self.background, (0, 0))
             else:
                 pygame.draw.rect(self.display, self.background_color, (0, 0, self.width, self.height))
-            collider.display()
-            player.display()
+            mainFrameUpdate()
+        elif scene == "Lose":
+            pygame.draw.rect(self.display, self.background_color, (0, 0, self.width, self.height))
+            loseFrameUpdate()
+        elif scene == "Win":
+            pygame.draw.rect(self.display, self.background_color, (0, 0, self.width, self.height))
+            winFrameUpdate()
         pygame.display.update()
 
 
@@ -81,11 +95,27 @@ class game_Action(object):
         self.exit = True
 
     def gameLoop(self):
-        while self.scene == "Main" and self.exit == False:
+        while self.exit == False:
+            if self.scene == "Main":
+                mainLoop()
+            elif self.scene == "Lose":
+                loseLoop()
+            elif self.scene == "Win":
+                winLoop()
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    gameWindow.gameExit()
+            gameWindow.frameUpdate(self.scene)
+        gameWindow.gameExit()
+        
+"""        while self.scene == "Main" and self.exit == False:
             gameWindow.frameUpdate(self.scene)
             mainLoop()
+        while self.scene == "Lose" and self.exit == False:
+            gameWindow.frameUpdate(self.scene)
+            loseLoop()
         gameWindow.gameExit()
-
+"""
 
 class button(object):
     def __init__(self, text):
@@ -99,13 +129,16 @@ class button(object):
         self.height = 100
         self.xPos = gameWindow.width/2
         self.yPos = gameWindow.height/2
+        self.box = True
+        self.angle = 0
 
-    def set_dimensions(self, width, height):
+    def set_size(self, width, height):
         self.width = width
         self.height = height
 
     def set_font_size(self, size):
         self.font_size = size
+        self.font = pygame.font.Font("freesansbold.ttf", self.font_size)
 
     def set_normal_color(self, color):
         self.normal_color = color
@@ -123,14 +156,23 @@ class button(object):
         self.xPos = xPos
         self.yPos = yPos
 
+    def no_box(self):
+        self.box = False
+
+    def rotate(self, angle):
+        self.angle = angle
+
     def display(self):
         #print ("Displaying", text, "button")
         disp_specs = ((self.xPos - (self.width/2)), (self.yPos - (self.height/2)), self.width, self.height)
         #print (position)
         textSurface = self.font.render(self.text, True, black)
+        if self.angle != 0:
+            textSurface = pygame.transform.rotate(textSurface, self.angle)
         textBox = textSurface.get_rect()
         textBox.center = (self.xPos, self.yPos)
-        pygame.draw.rect(gameWindow.display, self.normal_color, (disp_specs))
+        if self.box:
+            pygame.draw.rect(gameWindow.display, self.normal_color, (disp_specs))
         gameWindow.display.blit(textSurface, textBox)
 
 
@@ -139,16 +181,22 @@ class gamePiece(object):
         self.gravity = False
         self.collidable = False
         self.position = (0, 0)
+        self.orgPosition = (0, 0)
         self.length = 100
         self.height = 100
         self.hb_length = 100
         self.hb_height = 100
+        self.hb_type = "circle"
         self.triggered = False
         self.use_image = False
         self.sprite = ""
+        self.angle = 'v'
+        self.prevAngle = 'v'
+        self.speed = 1
 
     def set_pos(self, x, y):
         self.position = (x, y)
+        self.orgPosition = (x, y)
 
     def set_hitbox(self, length, height):
         self.hb_length = length
@@ -160,8 +208,8 @@ class gamePiece(object):
 
     def set_sprite(self, image):
         self.use_image = True
-        self.sprite = ""
-        self.sprite = pygame.transform.scale(self.sprite, (self.length, self.height))
+        self.sprite = pygame.image.load(image)
+        #self.sprite = pygame.transform.scale(self.sprite, (self.length, self.height))
         #self.background = pygame.transform.scale(self.background, (self.width, self.height))
         
     def set_size(self, length, height):
@@ -169,6 +217,12 @@ class gamePiece(object):
         self.height = height
         if self.use_image:
             self.sprite = pygame.transform.scale(self.sprite, (self.length, self.height))
+
+    def set_speed(self, speed):
+        self.speed = speed
+
+    def set_hb_type(self, hb_type):
+        self.hb_type = hb_type
 
     def get_hb(self):
         xPos = self.position[0]
@@ -179,21 +233,79 @@ class gamePiece(object):
         yMax = yPos + self.hb_height/2
         return (xMin, xMax, yMin, yMax)
 
-    def move_right(self, dist):
-        xPos = self.position[0] + dist
+    def orientUpdate(self):
+        if self.angle == 'v' and self.prevAngle != 'v':
+            self.sprite = pygame.transform.rotate(self.sprite, 90)
+        elif self.angle == 'h' and self.prevAngle != 'h':
+            self.sprite = pygame.transform.rotate(self.sprite, -90)
+        self.prevAngle = self.angle
+
+    def move_up(self):
+        self.angle = 'v'
+        xPos = self.position[0]
+        yPos = self.position[1] - self.speed
+        self.position = (xPos, yPos)
+        self.orientUpdate()
+
+    def move_down(self):
+        self.angle = 'v'
+        xPos = self.position[0]
+        yPos = self.position[1] + self.speed
+        self.position = (xPos, yPos)
+        self.orientUpdate()
+
+    def move_left(self):
+        self.angle = 'h'
+        xPos = self.position[0] - self.speed
         yPos = self.position[1]
         self.position = (xPos, yPos)
+        self.orientUpdate()
+
+    def move_right(self):
+        self.angle = 'h'
+        xPos = self.position[0] + self.speed
+        yPos = self.position[1]
+        self.position = (xPos, yPos)
+        self.orientUpdate()
 
     def get_triggered(self):
         return self.triggered
 
+    def chase(self, player):
+        player_xPos = player.position[0]
+        player_yPos = player.position[1]
+        self_xPos = self.position[0]
+        self_yPos = self.position[1]
+        if self_xPos > player_xPos:
+            self.move_left()
+        elif self_xPos < player_xPos:
+            self.move_right()
+        if self_yPos > player_yPos:
+            self.move_up()
+        elif self_yPos < player_yPos:
+            self.move_down()
+
     def collision(self, collider):
-        xPos = self.position[0]
-        yPos = self.position[1]
-        xMin = xPos - self.hb_length/2
-        xMax = xPos + self.hb_length/2
-        yMin = yPos - self.hb_height/2
-        yMax = yPos + self.hb_height/2
+        if self.hb_type == "circle":
+            x_dist = math.fabs(self.position[0] - collider.position[0])
+            y_dist = math.fabs(self.position[1] - collider.position[1])
+            maxRad = self.length/2 + collider.length/2
+            distance = math.sqrt(x_dist**2 + y_dist**2)
+            #print("maxRad =", maxRad, "  distance =", distance)
+            if distance <= maxRad:
+                self.triggered = True
+            else:
+                self.triggered = False
+                
+            if self.triggered == True:
+                print ("Collision detected")
+                return True
+        """
+        if self.hb_type == "rectangle":
+        xMin = self.position[0] - self.hb_length/2
+        xMax = self.position[0] + self.hb_length/2
+        yMin = self.position[1] - self.hb_height/2
+        yMax = self.position[1] + self.hb_height/2
         collider_hb = collider.get_hb()
         col_xMin = collider_hb[0]
         col_xMax = collider_hb[1]
@@ -204,11 +316,14 @@ class gamePiece(object):
         #print (collider_hb)
         if (xMin <= collider_hb[0] and xMax >= collider_hb[1] and yMin <= collider_hb[2] and yMax >= collider_hb[3]):
             self.triggered = True
-            print ("Collision detected")
+            #print ("Collision detected")
 
         elif (xMin <= collider_hb[1] and xMax >= collider_hb[0] and yMin <= collider_hb[3] and yMax >= collider_hb[2]):
             self.triggered = True
-            print ("Collision detected")
+            #print ("Collision detected")
+        else:
+            self.triggered = False
+        """
 
     def display(self):
         #print ("Displaying gamePiece")
@@ -222,30 +337,103 @@ class gamePiece(object):
             disp_specs = ((xPos - (self.length/2)), (yPos - (self.height/2)), self.length, self.height)
             pygame.draw.rect(gameWindow.display, black, (disp_specs))
 
-def mainLoop():
-        player.collision(collider)
-        for event in pygame.event.get():
-            if event.type == KEYDOWN and event.key == pygame.K_SPACE:
-                print("Space key Pressed")
-                collider.set_pos(200, 100)
-            if event.type == KEYDOWN and event.key == pygame.K_d:
-                player.move_right(5)
-            if event.type == QUIT:
-                print ("Trying to quit")
-                gameAction.exit = True
+    def reset(self):
+        self.position = self.orgPosition
+        self.angle = 'v'
 
+def mainLoop():
+    keys = pygame.key.get_pressed()
+    #print (player.position, collider.position)
+    player.collision(collider)
+    if keys[K_w]:
+        player.move_up()
+    if keys[K_a]:
+        player.move_left()
+    if keys[K_s]:
+        player.move_down()
+    if keys[K_d]:
+        player.move_right()
+    collider.chase(player)
+    if player.position[0] >= (gameWindow.width - 100):
+        print("You Win!")
+        gameAction.scene = "Win"
+        player.reset()
+        collider.reset()
+    if player.triggered:
+        gameAction.scene = "Lose"
+        player.reset()
+        collider.reset()
+
+def mainFrameUpdate():
+    gameWindow.background_color = spring_green
+    fps_display.display()
+    endzone.display()
+    collider.display()
+    player.display()
+
+def loseLoop():
+    keys = pygame.key.get_pressed()
+    if keys[K_SPACE]:
+        gameAction.scene = "Main"
+
+def loseFrameUpdate():
+    title.set_text("YOU LOSE!")
+    gameWindow.background_color = dim_red
+    fps_display.display()
+    title.display()
+    subheading.display()
+
+def winLoop():
+    keys = pygame.key.get_pressed()
+    if keys[K_SPACE]:
+        gameAction.scene = "Main"
+
+def winFrameUpdate():
+    title.set_text("YOU WIN!")
+    gameWindow.background_color = green
+    fps_display.display()
+    title.display()
+    subheading.display()
+    
 
 gameWindow = game_Window()
 gameAction = game_Action()
 
-gameWindow.set_res(500, 500)
+gameWindow.set_res(1280, 720)
 gameWindow.set_caption("Hello World")
 gameWindow.set_background_color(green)
 
 player = gamePiece()
-player.set_pos(100, 100)
+player.set_pos(100, gameWindow.height/2)
+player.set_sprite("Helm_Blue.png")
+player.set_speed(2)
 
 collider = gamePiece()
-collider.set_pos(300, 100)
+collider.set_pos((gameWindow.width - 100), (gameWindow.height/2))
+collider.set_sprite("Helm_Red.png")
+
+fps_display = button("FPS: ")
+fps_display.set_pos(50, 50)
+fps_display.no_box()
+
+endzone = button("Endzone")
+endzone.normal_color = white
+endzone.set_size(100, gameWindow.height)
+endzone.set_pos((gameWindow.width - 50), gameWindow.height/2) 
+endzone.set_font_size(32)
+endzone.rotate(-90)
+
+title = button("YOU WIN!")
+title.set_pos(gameWindow.width/2, gameWindow.height/2)
+title.set_font_size(72)
+title.no_box()
+title.set_size(400, 300)
+
+subheading = button("Press space to play again")
+subheading.set_pos(gameWindow.width/2, gameWindow.height/1.5)
+subheading.set_font_size(48)
+subheading.no_box()
+
+
 
 gameAction.gameLoop()
